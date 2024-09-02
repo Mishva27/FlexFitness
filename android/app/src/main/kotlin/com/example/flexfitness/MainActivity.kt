c0175903401a8b3495703f92a051d0e16e618cd0
Mishva Sankhavara <mishvasankhavara@gmail.com>
2024-09-02 17:43:10 +0530

    Add Project

---
+++ android/app/src/main/kotlin/com/example/flexfitness/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flexfitness
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/view/on_boarding/started_view.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+import '../../common/color_extension.dart';
+
+class StartedView extends StatefulWidget {
+  const StartedView({super.key});
+
+  @override
+  State<StartedView> createState() => _StartedViewState();
+}
+
+class _StartedViewState extends State<StartedView> {
+  @override
+  Widget build(BuildContext context) {
+    var media = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: TColor.white,
+      body: Container(
+        width: media.width,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                    colors: TColor.primaryG,
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight),
+          ),
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+
+                  const Spacer(),
+                  Text("Fitness", 
+                    style: TextStyle(
+                    color: TColor.black, 
+                    fontSize: 36, 
+                    fontWeight: FontWeight.w700
+                  ),
+                  ),
+
+                  Text("Everybody Can Train", 
+                    style: TextStyle(
+                    color: TColor.gray, 
+                    fontSize: 18, 
+                  ),),
+
+                  const Spacer(),
+
+                  MaterialButton(onPressed: (){}, 
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
+                    child: Text("Get Started", style: TextStyle(
+                    color: TColor.gray, 
+                    fontSize: 16,
+                    fontWeight: FontWeight.w700)
+                    ),
+                  ),
+
+                ],
+              )
+            ],
+          ),
+    ));
+  }
+}
